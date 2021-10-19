b3f8dc646315eaa66631ab85988a1d6ecbc33063
Shawn Kebel <fxrender@outlook.com>
2021-10-18 23:19:44 -0500

    Delete all data before seeding

--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,14 +1,25 @@
 import once from "./connections/once.js";
 import storeGoodsData from "./data.js";
 
-once
-  .connect()
-  .then((connection) =>
-    connection
-      .db("storeGoods")
-      .collection("storeGoods")
-      .insertMany(storeGoodsData)
-  )
-  .then(() => {
-    once.close();
-  });
+// Asynchronous Anonymous IIFE - Immediately Invoked Function Expression
+(async () => {
+  const conn = await once.connect();
+  await conn.db("storeGoods").collection("storeGoods").deleteMany({});
+  await conn
+    .db("storeGoods")
+    .collection("storeGoods")
+    .insertMany(storeGoodsData);
+  conn.close();
+})();
+
+// once
+//   .connect()
+//   .then((connection) =>
+//     connection
+//       .db("storeGoods")
+//       .collection("storeGoods")
+//       .insertMany(storeGoodsData)
+//   )
+//   .then(() => {
+//     once.close();
+//   });
