ee17addb3871fcd331680f3eb8643c19fb7ef053
Shawn Kebel <fxrender@outlook.com>
2021-10-23 23:16:26 -0500

    Rename collectionName to collection in config.js

--- server/app/config.js
+++ server/app/config.js
@@ -7,7 +7,7 @@ dotenv.config();
 export default {
   db: {
     clientURL: process.env.DB_CLIENT_URL,
-    collectionName: "storeGoods",
+    collection: "storeGoods",
     name: "storeGoods",
   },
   port: process.env.PORT || 3000,
--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -2,9 +2,9 @@ import config from "../config.js";
 import once from "./connections/once.js";
 import storeGoodsData from "./data.js";
 
-// Destructure 'db', then destructure 'name' and 'collectionName', and change collectionName to collection.
+// Destructure 'db', then destructure 'name' and 'collection'.
 const {
-  db: { name, collectionName: collection },
+  db: { name, collection },
 } = config;
 
 // Asynchronous Anonymous IIFE - Immediately Invoked Function Expression
